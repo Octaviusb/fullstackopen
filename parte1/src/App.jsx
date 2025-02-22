f8b2cc397d494a3477524eff2dff2283a9094f8d
Ener Octavio Buitrago Camelo <obuitragocamelo@yahoo.es>
2025-02-22 15:25:52 -0500

    primer ejercicio javascript

--- parte1/src/App.jsx
+++ parte1/src/App.jsx
@@ -1,54 +1,32 @@
-import PropTypes from 'prop-types';
-
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  );
-};
-
-Part.propTypes = {
-  name: PropTypes.string.isRequired,
-  exercises: PropTypes.number.isRequired,
-};
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map((part, index) => (
-        <Part key={index} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-Content.propTypes = {
-  parts: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      exercises: PropTypes.number.isRequired,
-    })
-  ).isRequired,
-};
-
 const App = () => {
-  const course = 'Half Stack application development';
-
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <h1>{course}</h1>
-      <Content parts={parts} />
-      <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
-    </div>
-  );
-};
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
 
-export default App;
+    </div>
+  )
+}
