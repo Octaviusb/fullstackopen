9373a50b76b700db591187a5bac9a1f181fe65a8
Ener Octavio Buitrago Camelo <obuitragocamelo@yahoo.es>
2025-02-22 15:12:26 -0500

    ejercicio react

--- parte1/src/App.jsx
+++ parte1/src/App.jsx
@@ -1,28 +1,54 @@
+import PropTypes from 'prop-types';
+
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+Part.propTypes = {
+  name: PropTypes.string.isRequired,
+  exercises: PropTypes.number.isRequired,
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+Content.propTypes = {
+  parts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      exercises: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
 const App = () => {
-    // const-definitions
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = 'Half Stack application development';
+
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
 
   return (
     <div>
       <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content parts={parts} />
+      <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
